25a87c33b9f5eaf97b187e6e18a9fbd76bba0cc9
KemoNine <mcrosson@kemonine.info>
2020-12-31 01:46:17 +0000

    Initial working bring up of nrf52840 mdk dongle

--- keyboards/zmk-config/config/boards/arm/nrf52840_mdk_dongle/mdk_dongle_power_led.c
+++ keyboards/zmk-config/config/boards/arm/nrf52840_mdk_dongle/mdk_dongle_power_led.c
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2020 The ZMK Contributors
+ * Copyright (c) 2016 Intel Corporation
  *
- * SPDX-License-Identifier: MIT
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr.h>
@@ -9,26 +9,30 @@
 #include <devicetree.h>
 #include <drivers/gpio.h>
 
-#define POWER_LED_NODE DT_ALIAS(led1_red)
+/* The devicetree node identifier for the "led0" alias. */
+#define LED_NODE DT_ALIAS(ledblue)
 
-#if DT_NODE_HAS_STATUS(POWER_LED_NODE, okay)
-#define POWER_LED DT_GPIO_LABEL(POWER_LED_NODE, gpios)
-#define PIN DT_GPIO_PIN(POWER_LED_NODE, gpios)
-#define FLAGS DT_GPIO_FLAGS(POWER_LED_NODE, gpios)
+#if DT_NODE_HAS_STATUS(LED_NODE, okay)
+#define LED DT_GPIO_LABEL(LED_NODE, gpios)
+#define PIN DT_GPIO_PIN(LED_NODE, gpios)
+#define FLAGS DT_GPIO_FLAGS(LED_NODE, gpios)
 #else
-#error "Unsupported board: power led devicetree alias is not defined"
-#define POWER_LED ""
-#define PIN 0
-#define FLAGS 0
+/* A build error here means your board isn't set up to blink an LED. */
+#error "Unsupported board: led devicetree alias is not defined"
+#define LED0	""
+#define PIN	0
+#define FLAGS	0
 #endif
 
 static int pwr_led_init(const struct device *dev) {
-	dev = device_get_binding(POWER_LED);
+	int ret;
+
+	dev = device_get_binding(LED);
 	if (dev == NULL) {
 		return -EIO;
 	}
 
-	int ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT | FLAGS);
 	if (ret < 0) {
 		return -EIO;
 	}
