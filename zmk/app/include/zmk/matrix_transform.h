d252bffb095ee11761429e0e8cc9ef703a5b0d2a
kemonine <kemonine@kemonine.info>
2021-01-05 07:14:00 -0500

    Remove ZMK sources ; mirrored elsewhere

--- zmk/app/include/zmk/matrix_transform.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2020 The ZMK Contributors
- *
- * SPDX-License-Identifier: MIT
- */
-
-#pragma once
-
-u32_t zmk_matrix_transform_row_column_to_position(u32_t row, u32_t column);
