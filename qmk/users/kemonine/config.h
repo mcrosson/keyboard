b4e53a4abba11de1db85237d6c8b012e67ea6d0f
KemoNine <kemonine@kemonine.info>
2022-01-22 03:12:09 -0500

    Remove unneeded code

--- qmk/users/kemonine/config.h
+++ qmk/users/kemonine/config.h
@@ -1,19 +1,7 @@
 #pragma once
 
-/**** TAP HOLD *****/
-#define TAPPING_TOGGLE 2
-#define PERMISSIVE_HOLD
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
-/****  END TAP HOLD *****/
-
 /******** MOUSE KEYS **********/
 #define MK_KINETIC_SPEED
-#define MOUSEKEY_DELAY         16
-#define MOUSEKEY_MOVE_DELTA    10
-#define MOUSEKEY_INITIAL_SPEED 10
-#define MOUSEKEY_BASE_SPEED    300
-/******** END MOUSE KEYS ***********/
 
 /***** Keyboard Definitions *****/
 #ifdef KEYBOARD_nullbitsco_tidbit
