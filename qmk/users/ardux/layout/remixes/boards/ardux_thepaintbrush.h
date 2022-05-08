3c97c3747b9cd977ccfe4405503c74328146b044
kemonine <kemonine@kemonine.info>
2022-05-08 16:34:15 -0400

    replace power keycode with home keycode

--- qmk/users/ardux/layout/remixes/boards/ardux_thepaintbrush.h
+++ qmk/users/ardux/layout/remixes/boards/ardux_thepaintbrush.h
@@ -4,7 +4,7 @@
 
 #define ARDUX_STD_LAYER_CUS_LEFT \
 STD_LEADING_NONES \
-KC_NO,            KC_VOLU,          KC_SYSTEM_POWER,        KC_WWW_BACK, \
+KC_NO,            KC_VOLU,          KC_WWW_HOME,        KC_WWW_BACK, \
 STD_BETWEEN_ROW_ONE_TWO_NONES \
-KC_TRNS,          KC_VOLD,          KC_WWW_SEARCH,          KC_MUTE      \
+KC_TRNS,          KC_VOLD,          KC_WWW_SEARCH,      KC_MUTE      \
 STD_TRAILING_NONES
