3cf222779df9815357bf8b45f0073fafd43dc1f2
KemoNine <kemonin@kemonine.info>
2024-03-21 11:05:20 -0400

    update ardux (big/40%) number layer to be more robust -- add dot, comma, enter, tab, underscore, space to the existing tweaks to facilitate number/date/time data entry

--- qmk/users/ardux/layout/remixes/remix_layers.h
+++ qmk/users/ardux/layout/remixes/remix_layers.h
@@ -9,13 +9,23 @@ STD_BETWEEN_ROW_ONE_TWO_NONES \
 KC_TRNS,          KC_VOLD,          KC_WWW_SEARCH,      KC_MUTE      \
 STD_TRAILING_NONES
 
+// Copy of needed defines from layers.h
+#ifndef ARDUX_BIG_LAYER_NUM_LEFT
+#ifdef ARDUX_TWO_THUMB
+#define ARDUX_BIG_LAYER_NUM_LEFT_THUMBS KC_UNDS, KC_SPACE
+#endif
+#ifndef ARDUX_TWO_THUMB
+#define ARDUX_BIG_LAYER_NUM_LEFT_THUMBS KC_TAB, KC_UNDS, KC_SPACE
+#endif
+#endif
+
 #define ARDUX_BIG_LAYER_NUM_LEFT \
 BIG_LEADING_NONES \
 KNUM_T4,          KNUM_T3,          KNUM_T2,          KNUM_T1,          KC_MINUS,            \
 BIG_BETWEEN_ROW_ONE_TWO_NONES \
 KNUM_B4,          KNUM_B3,          KNUM_B2,          KNUM_B1,          KC_SLASH,            \
 BIG_BETWEEN_ROW_TWO_THREE_NONES \
-KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_COLON,            \
+KC_NO,            KC_NO,            KC_NO,            KC_TAB,            KC_COLON,            \
 BIG_BETWEEN_ROW_THREE_THUMBS_NONES \
 ARDUX_BIG_LAYER_NUM_LEFT_THUMBS \
 BIG_TRAILING_NONES
