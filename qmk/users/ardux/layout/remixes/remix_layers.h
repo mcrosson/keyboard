5ac8c67cd003b712791598c373fa41722d84d686
KemoNine <kemonin@kemonine.info>
2024-03-21 10:20:38 -0400

    tweaks to number layer, combos to make date entry in org-mode/elsewhere far easier (use - for date separator

--- qmk/users/ardux/layout/remixes/remix_layers.h
+++ qmk/users/ardux/layout/remixes/remix_layers.h
@@ -8,3 +8,14 @@ KC_NO,            KC_VOLU,          KC_WWW_HOME,        KC_WWW_BACK, \
 STD_BETWEEN_ROW_ONE_TWO_NONES \
 KC_TRNS,          KC_VOLD,          KC_WWW_SEARCH,      KC_MUTE      \
 STD_TRAILING_NONES
+
+#define ARDUX_BIG_LAYER_NUM_LEFT \
+BIG_LEADING_NONES \
+KNUM_T4,          KNUM_T3,          KNUM_T2,          KNUM_T1,          KC_MINUS,            \
+BIG_BETWEEN_ROW_ONE_TWO_NONES \
+KNUM_B4,          KNUM_B3,          KNUM_B2,          KNUM_B1,          KC_NO,            \
+BIG_BETWEEN_ROW_TWO_THREE_NONES \
+KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            \
+BIG_BETWEEN_ROW_THREE_THUMBS_NONES \
+ARDUX_BIG_LAYER_NUM_LEFT_THUMBS \
+BIG_TRAILING_NONES
